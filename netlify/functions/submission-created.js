c25c9306a6f6134d13d50dbfa50b3071d4e66bdd
Zippy <zippyy@zoho.com>
2023-04-21 19:13:35 -0600

    Update submission-created.js

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,23 +1,24 @@
-require("dotenv").config()
+require("dotenv").config();
 
 import fetch from 'node-fetch';
-const { ELASTICEMAIL_API_KEY } = process.env
 
 exports.handler = async event => {
-  const payload = JSON.parse(event.body).payload
-  console.log(`Recieved a submission: ${payload.email}`)
+  const payload = JSON.parse(event.body).payload;
+  const apiUrl = `https://api.elasticemail.com/v2/contact/add?publicAccountID=61458a7b-ca00-4bb5-9e21-5db82d4846a0&email=${payload.email}`;
+  
+  console.log(`Received a submission: ${payload.email}`);
+  console.log(`API URL: ${apiUrl}`);
 
-  return fetch("https://api.elasticemail.com/v2/contact/add?publicAccountID=61458a7b-ca00-4bb5-9e21-5db82d4846a0", {
+  return fetch(apiUrl, {
     method: "POST",
     headers: {
-      Authorization: `Token ${ELASTICEMAIL_API_KEY}`,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email: payload.email, notes: payload.name }),
+    body: JSON.stringify({ notes: payload.name }),
   })
     .then(response => response.json())
     .then(data => {
-      console.log(`Submitted to ElasticEmail:\n ${data}`)
+      console.log(`Submitted to ElasticEmail:\n ${JSON.stringify(data)}`);
     })
-    .catch(error => ({ statusCode: 422, body: String(error) }))
-} 
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+}
