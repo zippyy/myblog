2e17e5e8095fd6b2d82817f370d323dc18ff59c2
Zippy <zippyy@zoho.com>
2023-04-21 19:31:31 -0600

    Update submission-created.js

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 
 exports.handler = async event => {
   const payload = JSON.parse(event.body).payload;
-  const apiUrl = `https://api.elasticemail.com/v2/contact/add?publicAccountID=61458a7b-ca00-4bb5-9e21-5db82d4846a0&email=${payload.email}&publicListID=E33EBA7A-C20D-4D3D-8F2F-5EEF42F58E6F&listName=blog`;
+  const apiUrl = `https://api.elasticemail.com/v2/contact/add?publicAccountID=61458a7b-ca00-4bb5-9e21-5db82d4846a0&email=${payload.email}&listName=blog`;
 
   console.log(`Received a submission: ${payload.email}`);
   console.log(`API URL: ${apiUrl}`);
